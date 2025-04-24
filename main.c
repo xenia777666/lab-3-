32f87ad2736602743c4920384624034aaa8ddf24
jsxenix <143598349+xenia777666@users.noreply.github.com>
2025-04-25 02:39:13 +0300

    Update main.c
    change

--- main.c
+++ main.c
@@ -1,32 +1,47 @@
-#include "startrec.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "functions.h"
+
+
+void start() {
+    int task;
+    printf("Выберите задание для проверки: ");
+    task = correct_choice(task);
+
+    while (1) {
+        if (task == 1) {
+            task_with_two_oldest_bits();
+            break;
+        } else if (task == 2) {
+            task_about_workers();
+            break;
+        } else {
+            printf("Неверный номер задания. Попробуйте еще раз: ");
+            task = correct_choice(task);
+        }
+    }
+}
+
 int main() {
-    int is_return = 1;
-    do {
-        printStart();
-
-        int choice;
-
-        while (scanf_s("%d", &choice) != 1 || getchar() != '\n' || choice > 2 || choice < 1)
-        {
-            printf("Error, input task number 1 or 2\n");
-            rewind(stdin);
+    int n = 2;
+    while (1) {
+        if (n == 2) {
+            start();
+            printf("\nЗакрыть программу?\n");
+            printf("1-Да\n2-Нет\n");
+            n = correct_choice(n);
         }
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            continue;
+        } else {
+            printf("Данного варианта ответа не существует. Повторите ввод: ");
+            n = correct_choice(n);
+        }
+    }
 
-        switch (choice) {
-            case 1:
-                task1();
-                break;
-            case 2:
-                task2();
-                break;
-            default:
-                printf("Ошибка.\n");
-                break;
-        }
-        printf("\n  Еще раз? 1 - да, другое - нет \n");
-        if (scanf_s("%d", &is_return) != 1 || is_return != 1 || getchar() != '\n') {
-            is_return = 0;
-        }
-    } while (is_return);
     return 0;
 }
